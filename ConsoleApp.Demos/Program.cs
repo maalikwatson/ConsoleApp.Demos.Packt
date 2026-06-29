ec9867443e07c85b748f0a03b7fa5655f4f0b834
Maalik. <160528317+maalikwatson@users.noreply.github.com>
2026-06-29 18:49:04 -0400

    Added first and last name prompts

--- ConsoleApp.Demos/Program.cs
+++ ConsoleApp.Demos/Program.cs
@@ -2,14 +2,20 @@
 Console.Write("Please enter your age: ");
 int age = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Please enter your name: ");
-string name = Convert.ToString(Console.ReadLine());
+Console.Write("Please enter your first name: ");
+string firstName = Convert.ToString(Console.ReadLine());
+
+Console.Write("Please enter your last name: ");
+string lastName = Convert.ToString(Console.ReadLine());
 
 // calculate retirement age
 int retirementAge = 65 - age;
 
 
 // print to console
+Console.WriteLine($"Full Name: {firstName} {lastName}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine("");
 Console.WriteLine($"Working years remaining: {retirementAge}.");
 
 Console.ReadKey();
