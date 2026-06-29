15c00571c6fa148503fea900c79a5c1c3430c20a
Maalik. <160528317+maalikwatson@users.noreply.github.com>
2026-06-29 18:43:20 -0400

    Add project files.

---
+++ ConsoleApp.Demos/Program.cs
@@ -0,0 +1,15 @@
+﻿// declare variables
+Console.Write("Please enter your age: ");
+int age = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Please enter your name: ");
+string name = Convert.ToString(Console.ReadLine());
+
+// calculate retirement age
+int retirementAge = 65 - age;
+
+
+// print to console
+Console.WriteLine($"Working years remaining: {retirementAge}.");
+
+Console.ReadKey();
